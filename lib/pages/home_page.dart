ae521c9ba3c810ea58c3b7ead8bad34063de11ab
juanadls <eleo.dev09@gmail.com>
2024-12-16 08:22:03 -0300

    beginner proyect added

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+
+import '../providers/scan_list_provider.dart';
+import '../widgets/custom_navigatorbar.dart';
+import '../widgets/scan_button.dart';
+import 'direcciones_page.dart';
+import 'mapas_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        title: Text('Historial'),
+        actions: [
+          IconButton(
+              icon: Icon(Icons.delete_forever),
+              onPressed: () {
+                Provider.of<ScanListProvider>(context, listen: false)
+                    .borrarTodos();
+              })
+        ],
+      ),
+      body: _HomePageBody(),
+      bottomNavigationBar: CustomNavigationBar(),
+      floatingActionButton: ScanButton(),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+}
+
+class _HomePageBody extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // Obtener el selected menu opt
+    final uiProvider = Provider.of<UiProvider>(context);
+
+    // Cambiar para mostrar la pagina respectiva
+    final currentIndex = uiProvider.selectedMenuOpt;
+
+    // Usar el ScanListProvider
+    final scanListProvider =
+        Provider.of<ScanListProvider>(context, listen: false);
+
+    switch (currentIndex) {
+      case 0:
+        scanListProvider.cargarScanPorTipo('geo');
+        return MapasPage();
+
+      case 1:
+        scanListProvider.cargarScanPorTipo('http');
+        return DireccionesPage();
+
+      default:
+        return MapasPage();
+    }
+  }
+}
